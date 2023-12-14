29e030512ec3d2c7b1ab501e0bd1b755bdf3cc76
dutanugraha82 <duta.nugraha82@gmail.com>
2023-12-14 19:47:51 +0700

    commenting routes

--- routes/web.php
+++ routes/web.php
@@ -26,37 +26,37 @@ Route::get('/faq', function(){
     return view('faq');
 });
 
-Route::get('/admin', function(){
-    return view('admin.contents.dashboard');
-});
+// Route::get('/admin', function(){
+//     return view('admin.contents.dashboard');
+// });
 
-Route::get('/login', [AdminCT::class,'login'])->name('login');
-Route::post('/login',[AdminCT::class,'authenticate']);
-Route::post('/logout',[AdminCT::class,'logout'])->middleware('auth');
+// Route::get('/login', [AdminCT::class,'login'])->name('login');
+// Route::post('/login',[AdminCT::class,'authenticate']);
+// Route::post('/logout',[AdminCT::class,'logout'])->middleware('auth');
 // Superadmin Route
-Route::middleware(['auth','superadmin','preventBack'])->prefix('/superadmin')->group(function(){
-    Route::get('/', [SuperAdminCT::class,'index'])->name('superadmin.dashboard');
-    Route::get('/admin-users', [SuperAdminCT::class,'adminUsers']);
-    Route::post('/admin-users/create', [SuperAdminCT::class,'storeAdminUsers']);
-    Route::get('/customers/rejected',[AdminCT::class,'rejectedCustomer'])->name('superadmin.rejectedCustomers');
-});
+// Route::middleware(['auth','superadmin','preventBack'])->prefix('/superadmin')->group(function(){
+//     Route::get('/', [SuperAdminCT::class,'index'])->name('superadmin.dashboard');
+//     Route::get('/admin-users', [SuperAdminCT::class,'adminUsers']);
+//     Route::post('/admin-users/create', [SuperAdminCT::class,'storeAdminUsers']);
+//     Route::get('/customers/rejected',[AdminCT::class,'rejectedCustomer'])->name('superadmin.rejectedCustomers');
+// });
 // Route End
 
 // Admin Route
-Route::middleware(['auth','admin','preventBack'])->prefix('/admin')->group(function(){
-    Route::get('/',[AdminCT::class,'index'])->name('admin.dashboard');
-    Route::put('/customer/{id}',[AdminCT::class,'validateCustomer']);
-    Route::get('/customers/validated',[AdminCT::class,'approvedCustomers'])->name('admin.approvedCustomers');
-    Route::delete('/customer/{id}',[AdminCT::class,'deleteCustomer']);
-    Route::get('/customers/rejected',[AdminCT::class,'rejectedCustomer'])->name('admin.rejectedCustomers');
-});
+// Route::middleware(['auth','admin','preventBack'])->prefix('/admin')->group(function(){
+//     Route::get('/',[AdminCT::class,'index'])->name('admin.dashboard');
+//     Route::put('/customer/{id}',[AdminCT::class,'validateCustomer']);
+//     Route::get('/customers/validated',[AdminCT::class,'approvedCustomers'])->name('admin.approvedCustomers');
+//     Route::delete('/customer/{id}',[AdminCT::class,'deleteCustomer']);
+//     Route::get('/customers/rejected',[AdminCT::class,'rejectedCustomer'])->name('admin.rejectedCustomers');
+// });
 // Route End
 
-Route::middleware(['auth','preventBack'])->group(function(){
-    Route::get('/profiles/edit',[AdminCT::class,'editProfile']);
-    Route::put('/profiles/{id}',[AdminCT::class,'updateProfile']);
-});
-Route::get('/booking', [CustomerController::class,'bookDate']);
-Route::post('/booking', [CustomerController::class,'bookDatePost']);
-Route::get('/booking/{id}', [CustomerController::class,'booking']);
-Route::post('/booking-store/{id}', [CustomerController::class,'storeBooking']);
+// Route::middleware(['auth','preventBack'])->group(function(){
+//     Route::get('/profiles/edit',[AdminCT::class,'editProfile']);
+//     Route::put('/profiles/{id}',[AdminCT::class,'updateProfile']);
+// });
+// Route::get('/booking', [CustomerController::class,'bookDate']);
+// Route::post('/booking', [CustomerController::class,'bookDatePost']);
+// Route::get('/booking/{id}', [CustomerController::class,'booking']);
+// Route::post('/booking-store/{id}', [CustomerController::class,'storeBooking']);
