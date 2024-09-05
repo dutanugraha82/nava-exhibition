a39bd3bc70f6b72a5be011beac40e2f86f39a2e1
Duta Nugraha <duta.nugraha82@gmail.com>
2024-09-05 09:08:18 +0700

    psychoforia nomor rekening

--- resources/views/users/order-request.blade.php
+++ resources/views/users/order-request.blade.php
@@ -3,18 +3,16 @@
     Psychoforia - order ticket
 @endsection
 @section('content')
-<div class="container-fluid" style="margin-top: 6rem;">
+<div class="container" style="margin-top: 6rem;">
     <h2 class="fs-montserrat mt-3 text-center text-white"><b>Pembayaran Tiket</b></h2>
     <div class="card p-4 fs-montserrat" style="border-radius:9px;">
         
         <form action="/tickets/{{ $data->kode_registrasi }}/payment/store" id="form" method="POST" enctype="multipart/form-data">
             @csrf
-           <div class="row">
-            <div class="col-md mb-4 mb-md-0">
-                <img class="d-block mx-auto" style="width: 20rem; height:25rem;" src="{{ asset('img/qris.JPG') }}" alt="qris">
-                <figcaption class="text-center">Scan QR untuk pembayaran <br> <a href="{{ asset('img/qris.JPG') }}" download>Download QRIS Great A Creative</a></figcaption>
-            </div>
-            <div class="col-md">
+            <p>Untuk pembayaran bisa dilakukan dengan cara transfer ke no rekening di bawah ini, lalu upload file bukti pembayaran di kolom yang sudah disediakan.</p>
+            <h5 style="font-family: Arial, Helvetica, sans-serif">1092917015 (A.N DEWI ANGGRAENI)</h5>
+            <button style="background-color: #6360e1; color:white" class="btn shadow-lg" onclick="salinNomor()">Salin Nomor Rekening</button>
+            <hr>
                 <div class="mb-3">
                     <label for="jumlah">Jumlah yang harus dibayar</label>
                     <input type="text" class="form-control" name="jumlah_tiket" value="{{ $total_harga }}" id="amount" max="2" readonly>
@@ -40,8 +38,6 @@
                 <div class="my-4">
                     <img class="img-preview d-block mx-auto" style="max-width: 300px" alt="">
                 </div>
-            </div>
-           </div>
             <button type="submit" class="btn d-block mx-auto" style="margin-top: 30px; background-color:#6360e1; color:white; width:90%;">Bayar</button>
         </form>
     </div>
@@ -50,5 +46,21 @@
 </div>
 @endsection
 @push('js')
+<script>
+    function salinNomor() {
+        // Membuat elemen teks sementara untuk menyalin nomor
+        var nomor = "1092917015";
+        var tempInput = document.createElement("input");
+        tempInput.value = nomor;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        tempInput.setSelectionRange(0, 99999); // Untuk perangkat mobile
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
+        
+        // Menampilkan notifikasi
+        alert("Nomor Rekening" + nomor + " berhasil disalin!");
+    }
+</script>
 <script src="{{ asset('js/app.js') }}"></script>
 @endpush
